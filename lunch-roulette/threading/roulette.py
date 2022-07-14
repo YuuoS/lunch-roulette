b19655ff9b69056e1a99054a0ab6ce147b4da47b
YuuoS <yukki94.27@gmail.com>
2022-07-14 11:13:09 +0900

    update

--- lunch-roulette/threading/roulette.py
+++ lunch-roulette/threading/roulette.py
@@ -9,7 +9,7 @@ class Roulette:
         self.shuffled_list = []
 
     def set_list(self, input_list):
-        self.input_list = input_list
+        self.input_list = input_list.copy()
 
     def normal_day(self):
         for row in range(int(len(self.input_list) / 3 * 7)):
@@ -20,11 +20,14 @@ class Roulette:
             self.input_list.append('学食')
 
     def tuesday(self):
-        for row in range(int(len(self.input_list) / 3 * 7 / 2)):
+        list_len = len(self.input_list)
+        for row in range(int(list_len / 3 * 3)):
             self.input_list.append('学食')
+        for row in range(int(list_len / 3 * 4)):
             self.input_list.append('テンホウ')
 
     def shuffling_list(self):
         self.shuffled_list = self.input_list.copy()
         random.shuffle(self.shuffled_list)
         random.shuffle(self.shuffled_list)
+
